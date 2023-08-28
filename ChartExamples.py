c8ffcea0df36845bd832392bc702f44347098d04
Andron67 <zizin.a@mail.ru>
2023-08-28 14:22:26 +0200

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,16 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Примеры простроения графиков")
+
+
+# Запуск цикла mainloop
+window.mainloop()
+
+
+
+
