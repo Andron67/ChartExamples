f7a8017b51f83db544e8dadbe4d5ff8fc4340074
Andron67 <zizin.a@mail.ru>
2023-08-30 11:08:12 +0200

    Добавление групп кнопка метка

--- ChartExamples.py
+++ ChartExamples.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # Импорт внешних файлов
 import chart1
 import chart2
-import chart3
+import chart8
 
 # Функция закрытия программы
 def do_close():
@@ -13,37 +13,72 @@ def do_close():
 
 # Создание главного окна
 window = tk.Tk()
-window.geometry("450x450")
+window.geometry("450x600")
 window.title("Примеры простроения графиков")
 
 # Добавление метки заголовка
 lblTitle = tk.Label(text = "Примеры построения графиков", font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
 lblTitle.place(x = 55, y = 25)
 
-# Добавление кнопки и метки для графика1
+# Добавление кнопки и метки для графика 1
 btnChart1 = tk.Button(window, text = "График 1", font = ('Helvetica', 10, 'bold'), command = chart1.plot_chart)
 btnChart1.place(x = 40, y = 115, width = 90, height = 30)
 
 lblChart1 = tk.Label(text = "График синуса matplotlib")
 lblChart1.place(x = 170, y = 122)
 
-# Добавление кнопки и метки для графика2
+# Добавление кнопки и метки для графика 2
 btnChart2 = tk.Button(window, text = "График 2", font = ('Helvetica', 10, 'bold'), command = chart2.plot_chart)
 btnChart2.place(x = 40, y = 165, width = 90, height = 30)
 
 lblChart2 = tk.Label(text = "Нормальное распределение")
 lblChart2.place(x = 170, y = 172)
 
-# Добавление кнопки и метки для графика3
-btnChart3 = tk.Button(window, text = "График 3", font = ('Helvetica', 10, 'bold'), command = chart3.plot_chart)
+# Добавление кнопки и метки для графика 3
+btnChart3 = tk.Button(window, text = "График 3", font = ('Helvetica', 10, 'bold'))
 btnChart3.place(x = 40, y = 215, width = 90, height = 30)
 
-lblChart3 = tk.Label(text = "Гистограмма")
+lblChart3 = tk.Label(text = "")
 lblChart3.place(x = 170, y = 222)
 
+# Добавление кнопки и метки для графика 4
+btnChart4 = tk.Button(window, text = "График 4", font = ('Helvetica', 10, 'bold'))
+btnChart4.place(x = 40, y = 265, width = 90, height = 30)
+
+lblChart4 = tk.Label(text = "График синуса matplotlib")
+lblChart4.place(x = 170, y = 272)
+
+# Добавление кнопки и метки для графика 5
+btnChart5 = tk.Button(window, text = "График 5", font = ('Helvetica', 10, 'bold'))
+btnChart5.place(x = 40, y = 315, width = 90, height = 30)
+
+lblChart5 = tk.Label(text = "Нормальное распределение")
+lblChart5.place(x = 170, y = 322)
+
+# Добавление кнопки и метки для графика 6
+btnChart6 = tk.Button(window, text = "График 6", font = ('Helvetica', 10, 'bold'))
+btnChart6.place(x = 40, y = 365, width = 90, height = 30)
+
+lblChart6 = tk.Label(text = "Гистограмма")
+lblChart6.place(x = 170, y = 372)
+
+# Добавление кнопки и метки для графика 7
+btnChart7 = tk.Button(window, text = "График 7", font = ('Helvetica', 10, 'bold'))
+btnChart7.place(x = 40, y = 415, width = 90, height = 30)
+
+lblChart7 = tk.Label(text = "График синуса matplotlib")
+lblChart7.place(x = 170, y = 422)
+
+# Добавление кнопки и метки для графика 8
+btnChart8 = tk.Button(window, text = "График 8", font = ('Helvetica', 10, 'bold'), command = chart8.plot_chart)
+btnChart8.place(x = 40, y = 465, width = 90, height = 30)
+
+lblChart8 = tk.Label(text = "Гистограмма")
+lblChart8.place(x = 170, y = 472)
+
 # Добавление кнопки закрытия программы
-btnClose = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
-btnClose.place(x = 330, y = 400, width = 90, height = 30)
+btnClose = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'))
+btnClose.place(x = 330, y = 550, width = 90, height = 30)
 
 # Запуск цикла mainloop
 window.mainloop()
